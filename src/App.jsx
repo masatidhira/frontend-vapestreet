6eb1b8359608bc318073ab8b7d28763943cc7987
masatidhira <masatidhira@gmail.com>
2023-05-27 12:48:13 +0700

    Add dropdown on header

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Navigation />
       <Header />
+      <Navigation />
     </>
   );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,16 +1,34 @@
+import { useState } from 'react';
 import { VscTriangleDown } from 'react-icons/vsc';
 import { RxAvatar } from 'react-icons/rx';
+import { CgLogOut } from 'react-icons/cg';
 
 import './Header.scss';
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="header">
       <div className="logo">VAPES STREET</div>
       <div className="dropdown">
         <RxAvatar className="avatar" />
-        <p>Vapes Street</p>
-        <VscTriangleDown />
+        <button className="dropdown-button" onClick={() => setIsOpen(!isOpen)}>
+          <p>Vapes Street</p>
+          <VscTriangleDown />
+        </button>
+      </div>
+      <div className={isOpen ? 'dropdown-menu isOpen' : 'dropdown-menu'}>
+        <ul>
+          <li className="mb-1">
+            <RxAvatar className="icon" />
+            <p>Profile</p>
+          </li>
+          <li>
+            <CgLogOut className="icon" />
+            <p>Logout</p>
+          </li>
+        </ul>
       </div>
     </header>
   );
